c335edd49e5896d6af1fe9f1ef680d02076e723a
SlowBubble <chorhanglam@gmail.com>
2025-07-20 16:44:44 -0400

    relative path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Set the base URL for assets to relative
   build: {
     outDir: 'docs', // Change the output directory from 'dist' to 'docs'
   },
